a881d138d7d14604d89ea74ee71836eaa52e1c39
Tech Shreyansh <165946497+TechyShreyansh@users.noreply.github.com>
2025-06-03 00:25:43 +0530

    Update string_gen.py

--- string_gen.py
+++ string_gen.py
@@ -1,4 +1,6 @@
-from pyrogram import Client, enums
+from telethon.sessions import StringSession
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import PeerUser
 
 micky_ = """
          _  ___    _   _  _     _ 
@@ -11,23 +13,21 @@ micky_ = """
                  (__)             
 
 Copyright (C) 2020-2023 by Tech-Shreyansh@Github, < https://github.com/TechyShreyansh >.
-This file is part of < https://github.com/TechyShreyansh/Pyrogram-Session-Gen > Project, and is released under the "GNU v3.0 License Agreement".
-Please see < https://github.com/TechyShreyansh/Pyrogram-Session-Gen/blob/main/LICENSE > All rights reserved.
+This file is part of < https://github.com/TechyShreyansh/MickyUB_Session-Gen > Project, and is released under the "GNU v3.0 License Agreement".
+Please see < https://github.com/TechyShreyansh/STRING-SESSION/blob/main/LICENSE > All rights reserved.
 """
 
 print(micky_)
 
-api_id = input("Enter Your API ID: \n")
+api_id = int(input("Enter Your API ID: \n"))
 api_hash = input("Enter Your API HASH : \n")
 
-with Client("my_account", api_id=api_id, api_hash=api_hash,
-            hide_password=True) as bot_:
-                  first_name = (bot_.get_me()).first_name
-                  string_session_ = f"<b><u>String Session For {first_name}</b></u> \n<code>{bot_.export_session_string()}</code>"
-                  bot_.send_message("me",
-                                    string_session_,
-                                    parse_mode=enums.ParseMode.HTML,
-                                    disable_web_page_preview=True)
+with TelegramClient(StringSession(), api_id, api_hash) as client:
+                  me = client.get_me()
+                  session_string = client.session.save()
+                  formatted_msg = f"<b><u>String Session For {me.first_name}</u></b>\n\n<code>{session_string}</code>"
+
+                  client.send_message("me", formatted_msg, parse_mode="html")
                   print(
-                      f"String Has Been Sent To Your Saved Message : {first_name}"
+                      f"String Has Been Sent To Your Saved Message : {me.first_name}"
                   )
